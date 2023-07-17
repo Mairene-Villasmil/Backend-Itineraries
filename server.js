3eb41e8b1786f994d7f61077c3f9786aa74a4541
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-07-17 12:09:12 -0300

    itineraries

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const Routes = require("./routes/routes.js")
 
 const app = express()
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4001
 // va a levantar en el puerto declarado en la variable de ambiente
 
 app.set("port", PORT) 
-
 app.use(cors())
-
 app.use(express.json())
 //estamos pidiendo la respuesta en formato json
 app.use("/api", Routes)
