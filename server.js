81492edab5f3d118610ee2125dd9ffd18c910e72
Mairene-Villasmil <mairenevillasmilf@gmail.com>
2023-07-18 15:21:13 -0300

    test

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ const PORT = process.env.PORT || 4000
 // va a levantar en el puerto declarado en la variable de ambiente
 
 app.set("port", PORT) 
+
 app.use(cors())
+
 app.use(express.json())
 //estamos pidiendo la respuesta en formato json
 app.use("/api", Routes)
 
 app.listen(PORT, ()=>{
-    
     console.log("SERVIDOR CORRIENDO EN EL PUERTO " + app.get("port"))
 })
